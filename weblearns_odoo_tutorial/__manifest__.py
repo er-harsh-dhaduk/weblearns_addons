62108aba287e6f14a0b84a51e48f340cfc011aed
Harsh Dhaduk <harshdhaduk@ip-10-10-104-43.ap-southeast-1.compute.internal>
2021-07-15 16:06:06 +0530

    odoo module added.

---
+++ weblearns_odoo_tutorial/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Weblearns Odoo 14 Technical / Development Tutorial',
+    'version': '14.0.0.1.0',
+    'summary': 'Odoo 14 Development Tutorial',
+    'sequence': -1,
+    'description': 'Odoo 14 Development Tutorial',
+    'category': 'Learn',
+    'author': 'Weblearns',
+    'maintainer': 'Weblearns',
+    'website': 'https://googlyengineer.blogspot.com',
+    'live_test_url': 'https://www.youtube.com/playlist?list=PLAR8TpPnVeTQi9q1vca1c52rnITF7wtwM',
+    'license': 'LGPL-3',
+    'depends': [
+        'website_slides'
+    ],
+    'data': [
+        'data/channel_data.xml',
+        'data/slide_data.xml'
+    ],
+    # 'images': ['static/description/banner.gif'],
+    'installable': True,
+    'application': True
+}
