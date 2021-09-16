074b2db15fcca5db4c590d986778a86b8f14bdc5
Harsh Dhaduk <eng.cmp.harsh@gmail.com>
2021-09-16 21:45:49 +0530

    Add cli commands tutorial

---
+++ odoo_cli_command_tutorial/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Odoo Development Tutorial',
+    'version': '14.0.0.1.0',
+    'summary': 'Odoo Development Tutorial',
+    'sequence': -111,
+    'description': 'Odoo 14 Development Tutorial',
+    'category': 'Tutorials',
+    'author': 'Weblearns',
+    'maintainer': 'Weblearns',
+    'website': 'https://googlyengineer.blogspot.com',
+    'live_test_url': 'https://www.youtube.com/playlist?list=PLAR8TpPnVeTQi9q1vca1c52rnITF7wtwM',
+    'license': 'LGPL-3',
+    'depends': [
+        'website_slides'
+    ],
+    'data': [
+        'data/channel_data.xml',
+        'data/slide_data.xml'
+    ],
+    'images': ['static/description/banner.gif'],
+    'installable': True,
+    'application': True
+}
